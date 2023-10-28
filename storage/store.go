e3c542aafe8198fcc6a6c8ed1fc30985d0b02bc6
Kumancev <dmitrijkumancev571@gmail.com>
2023-10-28 22:11:49 +0300

    changed store.go. added data storage logic

--- storage/store.go
+++ storage/store.go
@@ -1,1 +1,41 @@
 package storage
+
+import (
+	"encoding/json"
+	"io/fs"
+	"os"
+
+	"github.com/DmitriiKumancev/refactor-project/models"
+)
+
+const Store = "users.json"
+
+func InitStore() {
+	_, err := os.Stat(Store)
+	if os.IsNotExist(err) {
+		initialStore := models.UserStore{
+			Increment: 0,
+			List:      make(models.UserList),
+		}
+		SaveStore(initialStore)
+	}
+}
+
+func SaveStore(data models.UserStore) error {
+	b, err := json.Marshal(&data)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(Store, b, fs.ModePerm)
+	return err
+}
+
+func LoadStore() (models.UserStore, error) {
+	f, err := os.ReadFile(Store)
+	if err != nil {
+		return models.UserStore{}, err
+	}
+	var data models.UserStore
+	err = json.Unmarshal(f, &data)
+	return data, err
+}
