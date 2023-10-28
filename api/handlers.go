3e4bbf1e12f76a90af4c93c7549753baf6084d25
Kumancev <dmitrijkumancev571@gmail.com>
2023-10-28 22:18:00 +0300

    added handlers

--- api/handlers.go
+++ api/handlers.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"errors"
+	"net/http"
+)
+
+var (
+	ErrUserNotFound = errors.New("user_not_found")
+)
+
+func SearchUsers(w http.ResponseWriter, r *http.Request) {
+	//
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	//
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	//
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	//
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	//
+}
