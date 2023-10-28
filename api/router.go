ff7550faf712a6fd1bff08a08cff218f6996e12d
Kumancev <dmitrijkumancev571@gmail.com>
2023-10-28 22:45:42 +0300

    added logic to api/router.go. and main.go

--- api/router.go
+++ api/router.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Route("/users", func(r chi.Router) {
+				r.Get("/", SearchUsers)
+				r.Post("/", CreateUser)
+
+				r.Route("/{id}", func(r chi.Router) {
+					r.Get("/", GetUser)
+					r.Patch("/", UpdateUser)
+					r.Delete("/", DeleteUser)
+				})
+			})
+		})
+	})
+
+	return r
+}
