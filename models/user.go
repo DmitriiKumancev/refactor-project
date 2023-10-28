6f269a0e384d15d28d8550efcd4b4a8d71aaef10
Kumancev <dmitrijkumancev571@gmail.com>
2023-10-28 22:02:49 +0300

    changed models/user.go. defined models

--- models/user.go
+++ models/user.go
@@ -1,1 +1,44 @@
 package models
+
+import (
+	"errors"
+	"net/http"
+)
+
+type User struct {
+    CreatedAt   string `json:"created_at"`
+    DisplayName string `json:"display_name"`
+    Email       string `json:"email"`
+}
+
+type UserList map[string]User
+
+type UserStore struct {
+    Increment int      `json:"increment"`
+    List      UserList `json:"list"`
+}
+
+type CreateUserRequest struct {
+    DisplayName string `json:"display_name"`
+    Email       string `json:"email"`
+}
+
+func (c *CreateUserRequest) Bind(r *http.Request) error {
+    if c.DisplayName == "" {
+        return errors.New("display_name is required")
+    }
+
+    if c.Email == "" {
+        return errors.New("email is required")
+    }
+
+    return nil 
+}
+
+type UpdateUserRequest struct {
+    DisplayName string `json:"display_name"`
+}
+
+func (u *UpdateUserRequest) Bind(r *http.Request) error {
+    return nil
+}
